4b105c4bf82fbf6102d1e37ac41c3166c2185f55
devHksingh <hksingh.dev@gmail.com>
2024-07-13 15:56:48 +0530

    table

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useState } from "react"
+import NewMember from "./New"
+import { DataTable } from "../Datatable"
+import { columns } from "./Members/Columns"
 
 export default function Team(){
     const [members,setMembers] = useState<any>([
@@ -24,8 +27,18 @@ export default function Team(){
         },
     ])
     return (
-        <div>
-            Team component
+        <div className="grid gap-6 border rounded-lg shadow px-5 py-4 w-full max-w-[800px]">
+            <header className="flex items-start justify-between">
+                <div className="grid gap-1">
+                    <h1 className="text-2xl">Team</h1>
+                    <p>Invite new members in your team.</p>
+                </div>
+                <NewMember/>
+            </header>
+
+            <main>
+                <DataTable columns={columns} data={members}/>
+            </main>
         </div>
     )
 }
